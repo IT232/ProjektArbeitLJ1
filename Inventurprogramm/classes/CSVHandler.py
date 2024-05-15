07843124ebc376488f1070e17947d8ae66d35cef
Moon Shadow <moonshadowstory@gmx.de>
2024-05-15 09:35:52 +0200

    datenstrucktur ueberarbeiten

--- Inventurprogramm/classes/CSVHandler.py
+++ Inventurprogramm/classes/CSVHandler.py
@@ -1,21 +1,21 @@
 import csv
 import os
 
-class csv_handler:
+class CSVHandler:
 
-    FILENAME = 'database.csv'
+    FILENAME = 'Inventurprogramm/data/database.csv'
     TEMPFILENAME = 'temp.csv'
 
     @staticmethod
     def add_record(bezeichnung: str, typ: str, hersteller: str, anschaffungsdatum: str, anschaffungspreis: float, abteilung: str, standort: int):
-        with open(csv_handler.FILENAME, 'a', newline='') as csvfile:
+        with open(CSVHandler.FILENAME, 'a', newline='') as csvfile:
             record_writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             record_writer.writerow([bezeichnung] + [typ] + [hersteller] + [anschaffungsdatum] + [anschaffungspreis] + [abteilung] + [standort])
 
     @staticmethod
     def read_all_records():
         data_records = []
-        with open(csv_handler.FILENAME, 'r', newline='') as csvfile:
+        with open(CSVHandler.FILENAME, 'r', newline='') as csvfile:
             records_reader = csv.reader(csvfile, delimiter=';', quotechar='"')
             for row in records_reader:
                 data_records.append(row)
@@ -23,15 +23,15 @@ class csv_handler:
 
     @staticmethod 
     def delete_record(bezeichnung: str):
-        with open(csv_handler.FILENAME, 'r', newline='') as records_csvfile, \
-            open(csv_handler.TEMPFILENAME, 'a', newline='') as temp_csvfile:
+        with open(CSVHandler.FILENAME, 'r', newline='') as records_csvfile, \
+            open(CSVHandler.TEMPFILENAME, 'a', newline='') as temp_csvfile:
             records_reader = csv.reader(records_csvfile, delimiter=';', quotechar='"')
             temp_writer = csv.writer(temp_csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for row in records_reader:
                 if row[0]==bezeichnung:
                     continue
                 temp_writer.writerow(row)
-        os.remove(csv_handler.FILENAME)        
-        os.rename(csv_handler.TEMPFILENAME, csv_handler.FILENAME)
+        os.remove(CSVHandler.FILENAME)        
+        os.rename(CSVHandler.TEMPFILENAME, CSVHandler.FILENAME)
 
     
---
+++ Inventurprogramm/index.py
@@ -0,0 +1,5 @@
+from classes.CSVHandler import CSVHandler
+
+CSVHandler.add_record('N356', 'Notebook', 'Lenovo', '14.05.2024', 143.54, 'Personal', 4009)
+# print (CSVHandler.read_all_records())
+# CSVHandler.delete_record('N356')
