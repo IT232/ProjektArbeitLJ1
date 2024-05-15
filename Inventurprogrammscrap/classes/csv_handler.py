21a2ed75dd28acf87c0b7c8a153841eba882e020
Moon Shadow <moonshadowstory@gmx.de>
2024-05-15 06:49:06 +0200

    hinzufuegen Grundfunktionen read all - delete - add

--- Inventurprogrammscrap/classes/csv_handler.py
+++ Inventurprogrammscrap/classes/csv_handler.py
@@ -1,11 +1,37 @@
+import csv
+import os
+
 class csv_handler:
-    import csv
-    FILENAME = 'data/database.csv'
+
+    FILENAME = 'database.csv'
+    TEMPFILENAME = 'temp.csv'
 
     @staticmethod
-    def add_record(bezeichnung: str, typ: str, hersteller: str, anschaffungsdatum: str, anschaffungspreis: float, abteilung: str):
-        with open(csv_handler.FILENAME, 'w', newline='') as csvfile:
-            record_writer = csv.writer(csvfile, delimiter=' ',
-                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            record_writer.writerow([bezeichnung] + [typ] + [hersteller] + [anschaffungsdatum] + [anschaffungspreis] + [abteilung])
+    def add_record(bezeichnung: str, typ: str, hersteller: str, anschaffungsdatum: str, anschaffungspreis: float, abteilung: str, standort: int):
+        with open(csv_handler.FILENAME, 'a', newline='') as csvfile:
+            record_writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            record_writer.writerow([bezeichnung] + [typ] + [hersteller] + [anschaffungsdatum] + [anschaffungspreis] + [abteilung] + [standort])
+
+    @staticmethod
+    def read_all_records():
+        data_records = []
+        with open(csv_handler.FILENAME, 'r', newline='') as csvfile:
+            records_reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+            for row in records_reader:
+                data_records.append(row)
+        return data_records      
+
+    @staticmethod 
+    def delete_record(bezeichnung: str):
+        with open(csv_handler.FILENAME, 'r', newline='') as records_csvfile, \
+            open(csv_handler.TEMPFILENAME, 'a', newline='') as temp_csvfile:
+            records_reader = csv.reader(records_csvfile, delimiter=';', quotechar='"')
+            temp_writer = csv.writer(temp_csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for row in records_reader:
+                if row[0]==bezeichnung:
+                    continue
+                temp_writer.writerow(row)
+        os.remove(csv_handler.FILENAME)        
+        os.rename(csv_handler.TEMPFILENAME, csv_handler.FILENAME)
+
     
--- Inventurprogrammscrap/index.py
+++ Inventurprogrammscrap/index.py
@@ -1,3 +1,5 @@
 from classes.csv_handler import csv_handler
 
-csv_handler.add_record('N356', 'Notebook', 'Lenovo', '14.05.2024', 143.54, 'Personal')
+# csv_handler.add_record('N356', 'Notebook', 'Lenovo', '14.05.2024', 143.54, 'Personal', 4009)
+# print (csv_handler.read_all_records())
+# csv_handler.delete_record('N356')
